a31b2b1df884a31580fa99206a9070442ce09156
Arjun Tanwar <arjunthakur900@gmail.com>
2023-04-12 20:03:20 +0530

    react-query-devtools

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,13 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { client } from 'utils/trpc'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+    <ReactQueryDevtools />
+    <Component {...pageProps} />
+  </>
 }
 
 export default client.withTRPC(App)
