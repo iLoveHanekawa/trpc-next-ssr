da29a33546db7a7d867aab71ac0d50abf0c7f204
Arjun Tanwar <arjunthakur900@gmail.com>
2023-04-13 14:27:14 +0530

    finished

--- src/pages/artistsssr/index.tsx
+++ src/pages/artistsssr/index.tsx
@@ -1,3 +1,30 @@
+import { GetServerSideProps } from "next"
+import { createServerSideHelpers } from '@trpc/react-query/server'
+import { appRouter } from "@/server/routers/_app"
+import { client } from "utils/trpc"
+import { Artist } from "../artists"
 export default function ArtistsSSR() {
-    return <div>hello</div>
+
+    const { data, status } = client.artist.findmany.useQuery()
+    if(status !== 'success') return <div>Loading...</div>
+    return <ul>{(data as { artists: Artist[] }).artists.map((val, i) => {
+        return <li key = {i}>
+            <div>{val.id}</div>
+            <div>{val.name}</div>
+            <div>{val.description}</div>
+        </li>
+    })}</ul>
+}
+
+export const getServerSideProps: GetServerSideProps = async() => {
+    const helpers = createServerSideHelpers({
+        router: appRouter,
+        ctx: {}
+    })
+    await helpers.artist.findmany.prefetch()
+    return {
+        props: {
+            trpcState: helpers.dehydrate()
+        }
+    }
 }
